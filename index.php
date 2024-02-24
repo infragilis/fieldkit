82729b8632cdd8e325f228221f54626063e676aa
infragilis <infragilis@users.noreply.github.com>
2024-02-24 00:43:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+boonstra@fieldkit:/var/www/html $ vi index.php
 <!DOCTYPE html>
 <html>
     <head>
         <title>fieldkit</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
-
- <a href="http://localhost/data">data</a> </body>
+        <?php echo '<p>Hello fieldkit</p>'; ?>
+    <?php
+        $ip_server = $_SERVER['SERVER_ADDR'];
+        echo '<a href=/data target="_blank">Data</a>';
+    ?>
+</body>
 </html>
