d6e7985f3ca07008291e30a461f22792ee27118d
infragilis <infragilis@users.noreply.github.com>
2024-03-13 20:22:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ boonstra@fieldkit:/var/www/html $ vi index.php
     <?php
         $ip_server = $_SERVER['SERVER_ADDR'];
         echo '<a href=/data target="_blank">Data</a>';
+        echo '<a href=/usb target="_blank">Usb</a>';
     ?>
 </body>
 </html>
