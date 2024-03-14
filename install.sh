cdae49c3d5f66a185852c38666c400c198188102
infragilis <infragilis@users.noreply.github.com>
2024-03-13 20:18:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ sudo useradd -m -p $6$Jr6x2GOscd76/.7W$x9sVWXwLdNY0fzcnVQUR.wMLFfJKig/snUZC5b3wY
 sudo usermod -a -G sudo fieldkit
 #build some directories and stuff
 sudo mkdir /home/fieldkit/data
-sudo ln -s /home/fieldkit/data/ /var/www/html/
+sudo mkdir /home/fieldkit/www
+sudo mkdir /home/fieldkit/usb
+sudo ln -s /home/fieldkit/data/ /var/www/html/data
+sudo ln -s /home/fieldkit/usb/ /var/www/html/usb
 sudo chown -R root.fieldkit /home/fieldkit
-sudo chown -R root.www-data /var/www/html/
+sudo chown -R root.www-data /var/www/html 
+sudo mount /dev/sda1 /var/www/html/usb
 # upgrade to latest docker
 sudo wget -qO /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
@@ -17,7 +21,6 @@ sudo apt update
 sudo apt install -y php8.3-common php8.3-cli
 sudo apt install -y libapache2-mod-php8.3
 sudo cp apache2.conf /etc/apache2/
-sudo mkdir /home/fieldkit/www
 sudo mv index.php /var/www/html/
 sudo service apache2 restart
 
