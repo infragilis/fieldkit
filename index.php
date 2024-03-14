5689e613366933c892c1c5b1490ca147a92dd2ce
infragilis <infragilis@users.noreply.github.com>
2024-03-13 23:55:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
         <?php echo '<p>Hello fieldkit</p>'; ?>
     <?php
         $ip_server = $_SERVER['SERVER_ADDR'];
-        echo '<a href=/data target="_blank">Data</a>';
-        echo '<a href=/usb target="_blank">Usb</a><br>';
+        echo '<a href=/data target="_blank">Data</a></br>';
+        echo '<a href=/usb target="_blank">Usb</a></br>';
     ?>
 </body>
 </html>
